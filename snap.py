e97d6db10738348a5fb930c97aaff05c8d10b062
mazucanti <daniel.mazucanti.ismart@gmail.com>
2021-07-14 19:43:00 -0300

    Bitmap initialization implemented

--- snap.py
+++ snap.py
@@ -7,24 +7,35 @@ from pandas.core.series import Series
 
 class SNAP():
     def __init__(self):
-        self.nodes = pd.read_csv('data/nodes.csv')
+        self.nodes = pd.read_csv('data/nodes.csv', index_col=0)
         self.edges = pd.read_csv('data/edges.csv')
-        self.node_array = self.nodes['id_lattes']
+        self.node_array = self.nodes.index
 
     def generate_a_compatible_nodes(self, *atributes):
         attrs = list(atributes)
         a_compatible_nodes = self.nodes.sort_values(attrs)
         self.supernodes = self.nodes.groupby(attrs).groups
-        for key in self.supernodes:
-            self.supernodes[key] = set(self.supernodes[key])
-        print(self.supernodes)
-        self.bitmap = pd.DataFrame(
-            index=self.nodes['id_lattes'].to_list(), columns=self.supernodes.keys())
+        self._initialize_bitmap()
+        
         return a_compatible_nodes
 
-    def generate_a_compatible_edges(self, *atributes):
-        pass
+    def _initialize_bitmap(self):
+        self.bitmap = pd.DataFrame(
+            index=self.nodes.index.to_list(), columns=self.supernodes.keys())
+        for source in self.supernodes.keys():
+            self.bitmap[source] = 0
+            for target in self.supernodes.keys():
+                source_nodes = pd.Series(self.supernodes[source])
+                target_nodes = pd.Series(self.supernodes[target])
+                edges = self.edges.merge(
+                    source_nodes, left_on='source_id_lattes', right_on='id_lattes', how='inner')
+                edges = edges.merge(
+                    target_nodes, left_on='target_id_lattes', right_on='id_lattes', how='inner')
+                neighbour_nodes = edges['source_id_lattes'].to_list()
+                self.bitmap.loc[neighbour_nodes, source] = 1
+    
 
 
+nodes = pd.read_csv('data/nodes.csv', index_col=0)
 s = SNAP()
 s.generate_a_compatible_nodes('major_area')
